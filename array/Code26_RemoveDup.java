22c08dc8baf4bbe6a340bbbad1d34472db784e37
donghusanbao <hjwoohakim@163.com>
2020-10-07 20:49:18 +0800

    leetcode 26 remove duplicate

---
+++ array/Code26_RemoveDup.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Code26_RemoveDup {
+    //99% 18%
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                nums[++index] = nums[i];
+            }
+        }
+        return index + 1;
+    }
+}

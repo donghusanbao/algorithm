8b3aa9ca4e3723e002ae9a57f8c11ec260b7bad1
donghusanbao <hjwoohakim@163.com>
2020-10-21 17:07:50 +0800

    code 167 logN or N

--- array/Code167_TwoSum2.java
+++ array/Code167_TwoSum2.java
@@ -20,4 +20,41 @@ public class Code167_TwoSum2 {
         }
         return new int[]{0, 0};
     }
+    //二分法
+    public int[] twoSum2(int[] numbers, int target) {
+        if (numbers == null || numbers.length == 0) {
+            return new int[]{0, 0};
+        }
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (left == right - 1) {
+                return sum == target ? (new int[]{left + 1, right + 1}) : (new int[]{0, 0});
+            }
+            int mid = left + ((right - left) >> 1);
+            int leftSum = numbers[mid] + numbers[left];
+            int rightSum = numbers[mid] + numbers[right];
+            if (leftSum == target) {
+                return new int[]{left + 1, mid + 1};
+            } else if (leftSum > target) {
+                right = mid - 1;
+            } else {
+                if (rightSum == target) {
+                    return new int[]{mid + 1, right +1};
+                } else if (rightSum < target) {
+                    left = mid + 1;
+                } else {
+                    if (sum == target) {
+                        return new int[]{left + 1, right + 1};
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
 }

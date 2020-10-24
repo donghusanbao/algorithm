7acd8d06c6de7359f2e8febd745a20eef64f09a8
donghusanbao <hjwoohakim@163.com>
2020-10-25 00:05:12 +0800

    code 12 new method

--- string/Code12_IntToRoman.java
+++ string/Code12_IntToRoman.java
@@ -80,6 +80,21 @@ public class Code12_IntToRoman {
         }
     }
 
+    //方法二，时间复杂度为1
+    public String intToRoman2(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder result = new StringBuilder();
+        int res = num;
+        for (int i = 0; i < values.length && res > 0; i++) {
+            while (res >= values[i]) {
+                res -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         int[] value = {3245, 100, 50, 67, 773};
         Code12_IntToRoman intToRoman = new Code12_IntToRoman();
